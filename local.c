a8b790e824995f8b1125591ec028fa8d7600fc95
Tim Witvliet <t.witvliet@payter.nl>
2024-01-19 16:23:44 +0100

    wip

--- local.c
+++ local.c
@@ -1759,7 +1759,8 @@ static int foreach_in_dir(void *d, const char *path, bool is_dir,
 			ret = -errno;
 			iio_strerror(errno, buf, sizeof(buf));
 			IIO_ERROR("Unable to stat file: %s\n", buf);
-			goto out_close_dir;
+			continue;
+			// goto out_close_dir;
 		}
 
 		if (is_dir && S_ISDIR(st.st_mode) && entry->d_name[0] != '.') {
